62a7fac4770332d2d83e1544c5831ac51e118028
Roman Keanu Mufid <romankeanumufid@gmail.com>
2025-01-17 20:30:47 +0700

    authentication implemented

--- src/server.js
+++ src/server.js
@@ -1,5 +1,7 @@
 require("dotenv").config();
+
 const Hapi = require("@hapi/hapi");
+const Jwt = require("@hapi/jwt");
 
 const notes = require("./api/notes");
 const NotesService = require("./services/postgres/NotesService");
@@ -9,11 +11,17 @@ const users = require("./api/users");
 const UsersService = require("./services/postgres/UsersService");
 const UsersValidator = require("./validator/users");
 
+const authentications = require("./api/authentications");
+const AuthenticationsService = require("./services/postgres/AuthenticationsService");
+const TokenManager = require("./tokenize/TokenManager");
+const AuthenticationsValidator = require("./validator/authentications");
+
 const ClientError = require("./exceptions/ClientError");
 
 const init = async () => {
   const notesService = new NotesService();
   const usersService = new UsersService();
+  const authenticationsService = new AuthenticationsService();
 
   const server = Hapi.server({
     port: process.env.PORT,
@@ -39,21 +47,53 @@ const init = async () => {
     return h.continue;
   });
 
-  await server.register({
-    plugin: notes,
-    options: {
-      service: notesService,
-      validator: NotesValidator,
+  await server.register([
+    {
+      plugin: Jwt,
     },
+  ]);
+
+  server.auth.strategy("notesapp_jwt", "jwt", {
+    keys: process.env.ACCESS_TOKEN_KEY,
+    verify: {
+      aud: false,
+      iss: false,
+      sub: false,
+      maxAgeSec: process.env.ACCESS_TOKEN_AGE,
+    },
+    validate: (artifacts) => ({
+      isValid: true,
+      credentials: {
+        id: artifacts.decoded.payload.id,
+      },
+    }),
   });
 
-  await server.register({
-    plugin: users,
-    options: {
-      service: usersService,
-      validator: UsersValidator,
+  await server.register([
+    {
+      plugin: notes,
+      options: {
+        service: notesService,
+        validator: NotesValidator,
+      },
     },
-  });
+    {
+      plugin: users,
+      options: {
+        service: usersService,
+        validator: UsersValidator,
+      },
+    },
+    {
+      plugin: authentications,
+      options: {
+        authenticationsService,
+        usersService,
+        tokenManager: TokenManager,
+        validator: AuthenticationsValidator,
+      },
+    },
+  ]);
 
   await server.start();
   console.log(`Server berjalan pada ${server.info.uri}`);
--- src/services/postgres/UsersService.js
+++ src/services/postgres/UsersService.js
@@ -1,6 +1,7 @@
 const { Pool } = require("pg");
 const InvariantError = require("../../exceptions/InvariantError");
 const NotFoundError = require("../../exceptions/NotFoundError");
+const AuthenticationError = require("../../exceptions/AuthenticationError");
 const { nanoid } = require("nanoid");
 const bcrypt = require("bcrypt");
 
@@ -16,7 +17,7 @@ class UserService {
     const hashedPassword = await bcrypt.hash(password, 10);
 
     const query = {
-      text: 'INSERT INTO users VALUES($1, $2, $3, $4) RETURNING id',
+      text: "INSERT INTO users VALUES($1, $2, $3, $4) RETURNING id",
       values: [id, username, hashedPassword, fullname],
     };
 
@@ -53,10 +54,33 @@ class UserService {
     const result = await this._pool.query(query);
 
     if (!result.rows.length) {
-      throw new NotFoundError('User tidak ditemukan');
+      throw new NotFoundError("User tidak ditemukan");
     }
 
     return result.rows[0];
+  }
+
+  async verifyUserCredential(username, password) {
+    const query = {
+      text: "SELECT id, password FROM users WHERE username = $1",
+      values: [username],
+    };
+
+    const result = await this._pool.query(query);
+
+    if (!result.rows.length) {
+      throw new AuthenticationError("Kredensial yang anda berikan salah");
+    }
+
+    const { id, password: hashedPassword } = result.rows[0];
+
+    const isMatch = await bcrypt.compare(password, hashedPassword);
+
+    if (!isMatch) {
+      throw new AuthenticationError("Kredensial yang Anda berikan salah");
+    }
+    
+    return id;
   }
 }
 
